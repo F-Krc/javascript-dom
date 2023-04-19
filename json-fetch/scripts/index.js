bd283b3f0b918853bd6ee47f72e3d5de9733994c
Fatih Karaca <fatih.karaca@gmx.net>
2023-04-19 15:54:00 +0200

    edit

--- json-fetch/scripts/index.js
+++ json-fetch/scripts/index.js
@@ -10,12 +10,12 @@ const getCakes = () => {
   let html = '';
   for (let cake of parsedData.cakes) {
     html += `
-    <div class="card m-3 col-md-3">
+    <div class="card m-3" style="width: 18rem;">
         <img src=${cake.image} class="card-img-top" style="height:250px;">
         <div class="card-body ">
         <h2>${cake.title}</h2>
          <div class="border-bottom">${cake.author}</div>
-        <div style="height:100px">${cake.ingredients}</div>
+        <div style="height:100px">Ingredients: ${cake.ingredients.join(', ')}</div>
         </div>
     </div>
     `;
@@ -28,12 +28,12 @@ const getBiscuits = () => {
   let html = '';
   for (let biscuit of parsedData.biscuits) {
     html += `
-    <div class="card m-3 col-md-3">
+    <div class="card m-3" style="width: 18rem;">
         <img src=${biscuit.image} class="card-img-top" style="height:250px;">
         <div class="card-body ">
          <h2>${biscuit.title}</h2>
         <div class="border-bottom">${biscuit.author}</div>
-        <div style="height:100px">${biscuit.ingredients}</div>
+        <div style="height:100px">Ingredients: ${biscuit.ingredients.join(', ')}</div>
         </div>
     </div>
     `;
@@ -47,12 +47,12 @@ biscuits.innerHTML = getBiscuits();
 // the other way
 for (let breadd of parsedData.bread) {
   let card = `
-    <div class="card m-3 col-md-3">
+    <div class="card m-3" style="width: 18rem;">
         <img src=${breadd.image} class="card-img-top" style="height:250px;">
         <div class="card-body ">
         <h2>${breadd.title}</h2>
         <div class="border-bottom">${breadd.author}</div>
-        <div style="height:50px">${breadd.ingredients}</div>
+        <div style="height:50px">Ingredients: ${breadd.ingredients.join(', ')}</div>
         </div>  
     </div>
     `;
